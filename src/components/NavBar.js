3cd302277789da5c5957093e82bea1cb17e51d17
paula <paulalaorga@gmail.com>
2023-09-26 19:53:38 +1000

    all

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,15 @@
 import React from "react";
+import "../index.css";
+
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return (
+  <nav>
+    <a href="#home">Home</a>
+    <a href="#about">About</a>
+  </nav>
+  );
 }
 
 export default NavBar;
